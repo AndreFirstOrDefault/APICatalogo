33d718216464a565d3991a4e7b45ebebc2991905
andrejava2022 <andre.java.ads.2022@gmail.com>
2024-02-08 19:35:51 -0300

    Criado método Post

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -33,7 +33,7 @@ public class ProdutosController : Controller
         return produtos;
     }
 
-    [HttpGet("{id:int}")]
+    [HttpGet("{id:int}", Name= "ObterProduto")]
     public ActionResult<Produto> Get(int id)
     {
         var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
@@ -43,6 +43,18 @@ public class ProdutosController : Controller
         }
 
         return produto;
+    }
+
+    [HttpPost]
+    public ActionResult Post(Produto produto)
+    {
+        if (produto is null)
+            return BadRequest();
+
+        _context.Produtos.Add(produto);
+        _context.SaveChanges();
+        return new CreatedAtRouteResult("ObterProduto",
+            new {id=produto.ProdutoId},produto);
     }
 
 }
