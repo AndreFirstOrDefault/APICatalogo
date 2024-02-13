b4387c213279f7d17aaca0ce74edea2982af7f42
andrejava2022 <andre.java.ads.2022@gmail.com>
2024-02-13 11:47:52 -0300

    Usando Data Annotations

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -1,7 +1,6 @@
 ﻿using APICatalogo.Context;
 using APICatalogo.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers;
