a894508f684f34fbda4e856924e566cba73c7728
AndreFirstOrDefault <andre.java.ads.2022@gmail.com>
2024-02-14 19:16:25 -0300

    Criado repositório genérico

---
+++ APICatalogo/Repositories/Implements/Repository.cs
@@ -0,0 +1,52 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace APICatalogo.Repositories.Implements;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    // Não pode usar private pq precisa ser acessada pelas classes derivadas
+    protected readonly AppDbContext _context;
+
+    public Repository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        // Set -> retorna uma coleção ou 1 tabela
+        return _context.Set<T>().ToList();
+    }
+
+    public T? Get(Expression<Func<T, bool>> predicate)
+    {
+        return _context.Set<T>().FirstOrDefault(predicate);
+    }
+
+    public T Create(T entity)
+    {
+        _context.Set<T>().Add(entity);
+        _context.SaveChanges();
+        return entity;
+    }
+
+    public T Update(T entity)
+    {
+        // _context.Set<T>().Update(entity);
+        _context.Entry(entity).State = EntityState.Modified;
+        _context.SaveChanges();
+        return entity;
+    }
+
+    public T Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        _context.SaveChanges();
+        return entity;
+
+    }
+        
+}
---
+++ APICatalogo/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace APICatalogo.Repositories.Interfaces;
+
+public interface IRepository<T>
+{
+    // Cuidados para não violar o principio ISP
+    // Ex: Valor de estoque => categoria não tem estoque
+
+    IEnumerable<T> GetAll();
+
+    // Aceita uma função lambda e retorna um booleano
+    T? Get(Expression<Func<T, bool>> predicate);
+    T Create(T entity);
+    T Update(T entity);
+    T Delete(T entity);
+
+}
